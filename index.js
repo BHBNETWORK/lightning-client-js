bce8fa903035af3d1e048634db13d701415ea76a
Nadav Ivgi <nadav@shesek.info>
2017-10-21 12:29:53 +0300

    Improve debug message output control
    - Hide non-critical debug messages by default, allow to toggle it on/off
      using [debug](https://github.com/visionmedia/debug)
    
    - Write error messages to STDERR.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const net = require('net');
+const debug = require('debug')('lightning-client');
 const _ = require('lodash');
 
 class LightningClient {
@@ -12,7 +13,7 @@ class LightningClient {
 
         rpcPath = path.join(rpcPath, '/lightning-rpc');
 
-        console.log(`Connecting to ${rpcPath}`);
+        debug(`Connecting to ${rpcPath}`);
 
         this.rpcPath = rpcPath;
         this.reconnectWait = 0.5;
@@ -23,19 +24,19 @@ class LightningClient {
         this.client = net.createConnection(rpcPath);
         this.clientConnectionPromise = new Promise(resolve => {
             _self.client.on('connect', () => {
-                console.log(`Lightning client connected`);
+                debug(`Lightning client connected`);
                 _self.reconnectWait = 1;
                 resolve();
             });
 
             _self.client.on('end', () => {
-                console.log('Lightning client connection closed, reconnecting');
+                console.error('Lightning client connection closed, reconnecting');
                 _self.increaseWaitTime();
                 _self.reconnect();
             });
 
             _self.client.on('error', error => {
-                console.log(`Lightning client connection error`, error);
+                console.error(`Lightning client connection error`, error);
                 _self.increaseWaitTime();
                 _self.reconnect();
             });
@@ -108,7 +109,7 @@ class LightningClient {
         }
 
         this.reconnectTimeout = setTimeout(() => {
-            console.log('Trying to reconnect...');
+            debug('Trying to reconnect...');
 
             _self.client.connect(_self.rpcPath);
             _self.reconnectTimeout = null;
