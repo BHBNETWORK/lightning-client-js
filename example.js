c5ab0c30c60de9cdfe2fb42946fd0805acaf712f
Alekos Filini <alekos.filini@gmail.com>
2018-02-08 12:29:09 +0100

    Add a "debugFlag" to show real stacktraces when errors occur (#10)
    This squashes and merges two commits:
    
    * [acdaacb053f754a2316f67644bc8531c3db5be80] Add a "debugFlag" to show real stacktraces on errors
    * [159a311c2fb516824e7d835bab25e7c425a8ec77] Update README.md to use the new debug flag

--- example.js
+++ example.js
@@ -2,17 +2,18 @@
 
 const LightningClient = require('lightning-client');
 
-// This should point to your lightning-dir, by default in ~/.lightning
-const client = new LightningClient('/home/bitcoind/.lightning');
+// This should point to your lightning-dir, by default in ~/.lightning.
+// The debug mode is enabled (second parameter) but this decreases performances (see PR #10)
+const client = new LightningClient('/home/bitcoind/.lightning', true);
 
 // Every call returns a Promise
 
 // "Show information about this node"
 client.getinfo()
 	.then(info => console.log(info))
-	.catch(err => console.log(err.message));
+	.catch(err => console.log(err));
 
 // "Create an invoice for {msatoshi} with {label} and {description} with optional {expiry} seconds (default 1 hour)" }
 client.invoice(100, 'my-label-4', 'my-description-4', 3600)
 	.then(result => console.log(result))
-	.catch(err => console.log(err.message));
+	.catch(err => console.log(err));
